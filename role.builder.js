28a5c74315bc20c1e76a90b3c9a824889821e2a2
Farkes <chris.r.gendreau@gmail.com>
2019-03-10 05:21:00 -0400

    Changed rolebuilder to use WaitNGo

--- role.builder.js
+++ role.builder.js
@@ -2,32 +2,92 @@ var roleBuilder = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-
-        if(creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
-            creep.say('harvest');
-        }
-        if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.building = true;
-            creep.say('build');
+        if(creep.memory.dest){
+            var destination = Game.getObjectById(creep.memory.dest); // load destination from memory
         }
 
-        if(creep.memory.building) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+        creep.say(creep.memory.task);
+        
+        switch(creep.memory.task){
+            case "request":
+                destination = ""; 
+                if(creep.carry.energy == creep.carryCapacity){
+                    creep.memory.task = "bDestination";
+                } else {
+                    var sources = creep.room.find(FIND_SOURCES);
+                    for (i = 0; i< Memory.sourceInUse.length; i++){         //iterate over sources
+                        if(Memory.sourceInUse[i] < 1){         //check for available spot
+                            Memory.sourceInUse[i]++;
+                            destination = sources[i];
+                            creep.memory.sourceIndex = i;
+                            creep.memory.task = "harvest";
+                            break;
+                        }
+                    }
+                    if (destination == "") {                     //all in use, goto flag
+                        creep.memory.task = "fDestination";
+                    }
+                }
+            break;
+
+            case "bDestination": //set build destination
+                var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+                if(targets.length) {
+                    destination = creep.build(targets[0]);
+                    creep.memory.task = "deposit";
+                } else {
+                    creep.memory.task = "fDestination";
+                }
+            break;
+
+            case "fDestination": //set flag destination
+                creep.memory.flag = "Flag1";
+                creep.memory.task = "flag";
+            break;
+
+            case "flag": //Determine distance to flag and progress if outside
+            var flagdest = Game.flags[creep.memory.flag];
+            if(flagdest){
+                if(Math.sqrt((Math.pow((creep.pos.x-flagdest.pos.x),2)+Math.pow((creep.pos.y-flagdest.pos.y),2)))>5) {
+                    creep.moveTo(flagdest, {visualizePathStyle: {stroke: '#ffffff'}}); //move towards if over 5 away
+                } else {
+                    creep.memory.task = "request"; //request new task
                 }
             }
-        }
-        else {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                if(creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}}) == ERR_NO_PATH){
-                    creep.moveTo(Game.flags['Wait'], {visualizePathStyle: {stroke: '#ffaa00'}});   
+            break;
+
+            case "harvest": //progress to destination and harvest if energy is not full
+                if(creep.carry.energy<creep.carryCapacity){
+                    if(creep.harvest(destination) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(destination);
+                    }
+                } else {
+                    Memory.sourceInUse[creep.memory.sourceIndex]--;
+                    delete creep.memory.sourceIndex;
+                    creep.memory.task = "bDestination";
                 }
-            }
+            break;
+
+            case "deposit": //if has energy, progress to destination and transfer
+                if(creep.carry.energy>0){
+                    if(creep.transfer(destination, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                        creep.moveTo(destination, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                } else {
+                    creep.memory.task = "fDestination";
+                }
+            break;
+
+            default:
+                creep.memory.task = "request";
         }
+        //console.log('local: ' + destination);
+        if(destination){
+            //console.log('local destination recognized!');
+            creep.memory.dest = destination.id;
+        }
+        creep.memory.test = destination;
+        //console.log('remote mem: ' + creep.memory.dest);
     }
 };
 
