15fcbf02d655d6d5bb0d080f0403ebcf1fe2b5bb
Farkes <chris.r.gendreau@gmail.com>
2019-03-10 06:36:58 -0400

    Fixed spelling error

--- role.builder.js
+++ role.builder.js
@@ -40,7 +40,7 @@ var roleBuilder = {
                     targets = creep.room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_WALL || STRUCTURE_ROAD || STRUCTURE_TOWER }});
                     for(i = 0; i<targets.length; i++){
                         if(targets[i].hits < (targets[i].hitsMax * 0.33)){
-                            destination = targest[i];
+                            destination = targets[i];
                             break;
                         }
                     }
